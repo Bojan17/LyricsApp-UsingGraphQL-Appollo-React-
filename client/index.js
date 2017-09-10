86edc111bb3a2ce8766ba8a10a5c0a941b1bc47a
Bojan17 <bojan.kovacevic.pv@gmail.com>
2017-09-09 16:33:41 -0800

    inital commit

--- client/index.js
+++ client/index.js
@@ -1,8 +1,32 @@
+import './style/style.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ApolloClient from 'apollo-client';
+import {ApolloProvider} from 'react-apollo';
+import {Router, Route, hashHistory, IndexRoute} from 'react-router';
+
+import App from './components/App';
+import SongList from './components/SongList';
+import SongCreate from './components/SongCreate';
+import SongDetail from './components/SongDetail';
+
+const client = new ApolloClient({
+  //telling apollo to use ID to indetify data and keep track of it to tell React about updates
+  dataIdFromObject: o => o.id
+});
 
 const Root = () => {
-  return <div>Lyrical</div>
+  return (
+    <ApolloProvider client={client}>
+      <Router history={hashHistory}>
+        <Route path="/" component={App}>
+          <IndexRoute component={SongList} />
+          <Route path="songs/new" component={SongCreate} />
+        <Route path="songs/:id" component={SongDetail}/>
+        </Route>
+      </Router>
+    </ApolloProvider>
+  )
 };
 
 ReactDOM.render(
